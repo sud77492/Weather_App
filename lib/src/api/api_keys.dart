883adc691105f1dfbef8e48049813f4ee8b27a01
sud7749 <sameersharma078626@gmail.com>
2020-07-21 22:58:54 +0800

    first commit

---
+++ lib/src/api/api_keys.dart
@@ -0,0 +1,3 @@
+class ApiKey {
+  static String OPEN_WEATHER_MAP = "1a4e92b1ab7801add2cbe68c18b2df13";
+}
---
+++ lib/src/screens/routes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_carsome/src/screens/settings_screen.dart';
+import 'package:weather_app_carsome/src/screens/weather_screen.dart';
+
+class Routes {
+  static final mainRoute = <String, WidgetBuilder>{
+    '/home': (context) => WeatherScreen(),
+    '/settings': (context) => SettingsScreen(),
+  };
+}
